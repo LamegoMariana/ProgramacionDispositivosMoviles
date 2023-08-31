7996cc30ffab337e24f524b58ab5423ade6e6d97
LAMEGO SORIANO <mariana.lamegos@iteso.mx>
2023-08-31 07:09:35 -0600

    Botones cambian de color al ser presionados (IconButton, onPressed, _ispressed)

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isPressed1 = false;
+  bool _isPressed2 = false;
+  bool _isPressed3 = false;
+  bool _isPressed4 = false;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,7 +25,7 @@ class MyApp extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Container(
-            height: 120,
+            height: 150,
             decoration: BoxDecoration(border: Border.all(color: Colors.black)),
             child: Padding(
               padding: const EdgeInsets.all(8.0),
@@ -65,15 +74,42 @@ class MyApp extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Spacer(flex: 1),
-                      Icon(Icons.accessibility_rounded),
-                      Spacer(flex: 1),
-                      Icon(Icons.timer),
-                      Spacer(flex: 1),
-                      Icon(Icons.phone_android),
-                      Spacer(flex: 1),
-                      Icon(Icons.phone_iphone),
-                      Spacer(flex: 1)
+                      IconButton(
+                          icon: Icon(Icons.accessibility_rounded),
+                          onPressed: () {
+                            setState(() {
+                              _isPressed1 =
+                                  !_isPressed1; // Cambia el estado al presionar
+                            });
+                          },
+                          color: _isPressed1 ? Colors.indigo : Colors.black),
+                      IconButton(
+                          icon: Icon(Icons.timer),
+                          onPressed: () {
+                            setState(() {
+                              _isPressed2 =
+                                  !_isPressed2; // Cambia el estado al presionar
+                            });
+                          },
+                          color: _isPressed2 ? Colors.indigo : Colors.black),
+                      IconButton(
+                          icon: Icon(Icons.phone_android),
+                          onPressed: () {
+                            setState(() {
+                              _isPressed3 =
+                                  !_isPressed3; // Cambia el estado al presionar
+                            });
+                          },
+                          color: _isPressed3 ? Colors.indigo : Colors.black),
+                      IconButton(
+                          icon: Icon(Icons.phone_iphone),
+                          onPressed: () {
+                            setState(() {
+                              _isPressed4 =
+                                  !_isPressed4; // Cambia el estado al presionar
+                            });
+                          },
+                          color: _isPressed4 ? Colors.indigo : Colors.black),
                     ],
                   )
                 ],
